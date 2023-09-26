e379a177f082ebdf2ef30667bb18dd5caed0daf1
youssef-abjaou <130617788+youssef-abjaou@users.noreply.github.com>
2023-09-26 13:31:13 +0100

    delete unnecessary files

--- 3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        value = a / b
-    except Exception:
-        value = None
-    finally:
-        print("Inside result: {}".format(value))
-    return value
